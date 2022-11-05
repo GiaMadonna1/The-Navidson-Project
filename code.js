7423fa244cd8425179e6e4f9afa745a2cc451e62
Gia Madonna <giamadonna1@gmail.com>
2022-11-05 14:29:46 -0700

    Each cell now has an Id. Trying to figure out why it will work in my console and not on the live website

--- code.js
+++ code.js
@@ -1,14 +1,17 @@
 var timer;
-var timeE = document.getElementById('Timer');
+let timeE = document.getElementById('Timer');
 
-function countdownTimer(){
-    var sec = 30;
-    timer = setInterval (() => {
+ function countdownTimer(){     
+    let sec = 30;
+     timer = setInterval (() => {
         timeE.innerHTML = '00:'+ sec;
         sec --;
     }, 1000)
-}
-setInterval(countdownTimer, 1000);
+
+    if (sec === 0){
+       clearInterval()
+    }
+ }
 
 document.addEventListener('click', startTheGame)
 
@@ -21,19 +24,28 @@ function createTabMaze(){
     var colIndex = 0;
     let table = document.querySelector('.codeThis');
     let mazeLayout = document.createElement('tbody');
-        for (i = rowIndex; i <= 11; i++){
+        for (i = rowIndex; i <= 10; i++){
             let row = document.createElement('tr');
             
-            for (j = colIndex; j <= 15; j++){
-                let sideRows = document.createElement('td');
-                sideRows.style.width = '5em'
-                sideRows.style.height = '3.9em'
-                sideRows.style.backgroundColor = 'white'
-                row.appendChild(sideRows)
+            for (j = colIndex; j <= 10; j++){
+                let cell = document.createElement('td');
+                cell.setAttribute('id', `${i}${j}`)
+                cell.setAttribute('class', "cellStyle")
+                console.log(cell)
+                row.appendChild(cell)
             }
             table.appendChild(row)
         }
 
         document.body.appendChild(mazeLayout)
+
+        
 }
 
+cell.addEventListener('dblclick', Dubb)
+
+function Dubb (mouseEvent, createTabMaze){
+   if  (cell === ondblclick) {
+    cell.style.backgroundColor = 'yellow'
+   }
+}
