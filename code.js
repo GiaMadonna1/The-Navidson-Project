81ee6b5ff6002c8b45360d8385fad22667f6b196
Gia Madonna <giamadonna1@gmail.com>
2022-11-07 16:09:54 -0800

    Made the functionality better. Couldn't get the win/lose yet

--- code.js
+++ code.js
@@ -17,7 +17,7 @@ function createTabMaze(){
                 let cell = document.createElement('td');
                 cell.setAttribute('id', 'cell'+`${i}${j}`)
                 cell.setAttribute('class', "cellStyle")
-                cell.ondblclick = (e) => {
+                cell.onclick = (e) => {
                     cell.style.backgroundColor = '#B84437'
                 }
                 console.log(cell)
@@ -33,5 +33,11 @@ function createTabMaze(){
 
 
 
+function winTheGame() {
+    let winningSequence = ['#cell00', '#cell10', '#cell20', '#cell21', '#cell31', '#cell41', '#cell42', '#cell43', '#cell44', '#cell45', '#cell46', '#cell56', '#cell66', '#cell67', '#cell77', '#cell78', '#cell79', '#cell89', '#cell99', '#cell910', "cell1010"]
+        if (winningSequence.style.backgroundColor = '#B84437'){
+            createTabMaze.remove()
 
+        }
+}
 
